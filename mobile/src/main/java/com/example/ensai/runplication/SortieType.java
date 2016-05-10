9df4a1aca4306e988fe0464b34a65b6ac28a5a84
Evargalo <olivier.pucher@ensai.fr>
2016-05-10 12:30:33 +0200

    coucou

---
+++ mobile/src/main/java/com/example/ensai/runplication/SortieType.java
@@ -0,0 +1,15 @@
+package com.example.ensai.runplication;
+
+/**
+ * Created by ensai on 10/05/16.
+ */
+public enum SortieType {
+    marche,
+    footing,
+    fartlek,
+    sortie_longue,
+    competition,
+    fractionne_court,
+    fractionne_intermediaire,
+    fractionne_long;
+}
