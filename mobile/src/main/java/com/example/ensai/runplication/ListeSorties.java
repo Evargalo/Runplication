38ef2224d4db9d8f5d2d2c0a39422d7c78f5ceb9
Evargalo <olivier.pucher@ensai.fr>
2016-05-10 15:36:50 +0200

    coucou

---
+++ mobile/src/main/java/com/example/ensai/runplication/ListeSorties.java
@@ -0,0 +1,60 @@
+package com.example.ensai.runplication;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.ensai.runplication.Database.Database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListeSorties extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button button = null;
+
+    public void onClick(View v){
+        Toast.makeText(this,R.string.textOnClick,Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(this, Sortie.class);
+        // myIntent.putExtra("key", value); //Optional parameters
+        this.startActivity(myIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_sorties);
+
+
+        List<Sortie> sortieList =new ArrayList<Sortie>();
+        Database myDB=new Database(this);
+        String[] arguments={};
+        // myDB.getReadableDatabase().rawQuery("SELECT * FROM task;", arguments);
+
+
+        Cursor c = myDB.getReadableDatabase().rawQuery("SELECT * FROM sortie", arguments);
+        Sortie sortie=null;
+        int nbrows=c.getCount();
+        while(c.moveToNext())
+        {
+            sortie=new Sortie(this);
+            sortie.setId(c.getInt(0));
+            sortieList.add(sortie);
+        }
+        c.close();
+
+        ArrayAdapter<Sortie> sortieTable =new ArrayAdapter<Sortie>(this,android.R.layout.simple_list_item_1,android.R.id.text1,sortieList);
+
+        ListView listOfSorties=(ListView) findViewById(R.id.listeDesSorties);
+        listOfSorties.setAdapter(sortieTable);
+    }
+
+}
+
